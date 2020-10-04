a4da330c6ec14fe03d37755d510c72cc20426c3f
Rishi Venkatesh <70112927+Ethyx@users.noreply.github.com>
2020-10-04 09:50:45 -0500

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,34 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        
+        if (this.body.speed < 3)
+        {
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+        }
+        else
+        {
+        World.remove(world, this.body);
+        push();
+        this.Visibility = this.Visibility - 5;
+        tint(255, this.Visibility);
+        pop();
+       }
+      }
+}
